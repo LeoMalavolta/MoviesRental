3ceb738bde69057d19194fc4bcfedc6c6bc165bb
LeoMalavolta <ldmalavolta@gmail.com>
2025-11-18 07:26:30 -0300

    Domain Objects

---
+++ src/BuildingBlocks/MoviesRental.Core/DomainObjects/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesRental.Core.DomainObjects
+{
+    public class Entity
+    {
+        public Guid Id { get; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+        public DateTime DeletedAt { get; protected set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
