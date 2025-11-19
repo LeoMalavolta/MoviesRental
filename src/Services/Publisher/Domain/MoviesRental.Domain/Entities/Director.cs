6fbcc8e97dfb732a8c7cd756b5f1347cf11bc6b9
LeoMalavolta <ldmalavolta@gmail.com>
2025-11-18 23:04:47 -0300

    Dvd Entity

---
+++ src/Services/Publisher/Domain/MoviesRental.Domain/Entities/Director.cs
@@ -0,0 +1,53 @@
+﻿using MoviesRental.Core.DomainObjects;
+using System.Text.RegularExpressions;
+
+namespace MoviesRental.Domain.Entities
+{
+    public class Director : Entity
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        private List<Dvd> _dvds = new List<Dvd>();
+        public IReadOnlyList<Dvd> Dvds => _dvds;
+
+
+        public const int MIN_NAME_LENGTH = 3;
+        public const int MAX_NAME_LENGTH = 30;
+
+
+        public string FullName => $"{Name} {Surname}";
+
+        protected Director() { }
+
+        public Director(string name, string surname)
+        {
+            UpdateName(name);
+            UpdateSurname(surname);
+        }
+
+        public void UpdateName(string name)
+        {
+            if(!ValidateName(name))
+                throw new DomainException($"Invalid name for director.");
+
+            Name = name;
+        }
+
+        public void UpdateSurname(string surname)
+        {
+            if (!ValidateName(surname))
+                throw new DomainException($"Invalid surname for director.");
+
+            Surname = surname;
+        }
+
+        private bool ValidateName(string value)
+        {
+            if (string.IsNullOrEmpty(value) || value.Length < MIN_NAME_LENGTH || value.Length > MAX_NAME_LENGTH)
+                return false;
+
+            return Regex.IsMatch(value, "^(?=.*[A-ZÀ-ÿ~])(?=.*[a-zà-ÿ~])[A-Za-zÀ-ÿ~]+$");
+        }
+    }
+}
---
+++ src/Services/Publisher/Domain/MoviesRental.Domain/Entities/Dvd.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesRental.Domain.Entities
+{
+    internal class Dvd
+    {
+    }
+}
